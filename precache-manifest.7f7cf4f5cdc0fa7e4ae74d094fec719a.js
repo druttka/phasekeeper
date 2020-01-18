7c800e5c1c7b3d96d121c7de2c55be2e323a4de7
David Ruttka <david.ruttka@microsoft.com>
2020-01-17 16:52:12 -0800

    Initial build

---
+++ precache-manifest.7f7cf4f5cdc0fa7e4ae74d094fec719a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cc060c952ee7bb681b5d7421e5e0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53e58e5922002a254e37",
+    "url": "/static/css/main.ea9e797f.chunk.css"
+  },
+  {
+    "revision": "213906128bfe04d4bd74",
+    "url": "/static/js/2.fe3bae45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.fe3bae45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53e58e5922002a254e37",
+    "url": "/static/js/main.41acee93.chunk.js"
+  },
+  {
+    "revision": "88816c0063cd4526852f",
+    "url": "/static/js/runtime-main.3e95deca.js"
+  }
+]);
