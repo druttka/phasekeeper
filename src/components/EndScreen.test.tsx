f07e6ac4960e9927d612831af658910f6ea86061
David Ruttka <david@davidruttka.com>
2021-11-13 08:04:57 -0800

    chore: add failing test to demonstrate exploding component

---
+++ src/components/EndScreen.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import { EngineProvider } from "../state";
+import { EndScreen } from "./EndScreen";
+
+describe("EndScreen", () => {
+  describe("with no players", () => {
+    test("it renders an error view", () => {
+      const { getByTestId } = render(
+        <EngineProvider
+          initialState={{
+            gameState: "complete",
+            hasGameStarted: true,
+            isRoundInProgress: false,
+            phasesView: "active",
+            players: [],
+          }}
+        >
+          <EndScreen />
+        </EngineProvider>
+      );
+
+      const errorText = getByTestId("error-view");
+      expect(errorText).toHaveLength(1);
+    });
+  });
+});
