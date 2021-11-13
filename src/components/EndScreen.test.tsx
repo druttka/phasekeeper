720c3c6b8ec06f39af7f23e48fc86a1f4bf025d9
David Ruttka <david@davidruttka.com>
2021-11-13 08:20:44 -0800

    chore: add tests for EndScreen sorting

--- src/components/EndScreen.test.tsx
+++ src/components/EndScreen.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import { EngineProvider } from "../state";
+import { EngineProvider, EngineState, PlayerState } from "../state";
 import { EndScreen } from "./EndScreen";
 
 describe("EndScreen", () => {
@@ -23,6 +23,81 @@ describe("EndScreen", () => {
 
       const errorText = getByTestId("error-view");
       expect(errorText).toHaveTextContent("Whoops");
+    });
+  });
+
+  describe("with all players completing Phase 10", () => {
+    let state: EngineState;
+
+    const createFinishedPlayerState = (
+      id: number,
+      score: number
+    ): PlayerState => ({
+      completedPhase: 10,
+      lastCommittedScore: score,
+      playerId: id,
+      name: `Player ${id}`,
+      stagedScoreAdjustment: 0,
+    });
+
+    beforeEach(() => {
+      state = {
+        gameState: "complete",
+        hasGameStarted: true,
+        isRoundInProgress: false,
+        phasesView: "active",
+        players: [
+          createFinishedPlayerState(1, 25),
+          createFinishedPlayerState(2, 50),
+          createFinishedPlayerState(3, 10),
+        ],
+      };
+    });
+
+    test("has congratulations text", () => {
+      const { getByTestId } = render(
+        <EngineProvider initialState={state}>
+          <EndScreen />
+        </EngineProvider>
+      );
+
+      const congratulationsText = getByTestId("text-congrats");
+      expect(congratulationsText).toHaveTextContent("Congratulations");
+    });
+
+    test("congratulates the player with the lowest score", () => {
+      const { getByTestId } = render(
+        <EngineProvider initialState={state}>
+          <EndScreen />
+        </EngineProvider>
+      );
+
+      const congratulationsText = getByTestId("text-congrats");
+      expect(congratulationsText).toHaveTextContent("Player 3");
+    });
+
+    test("lists all players", () => {
+      const { getAllByTestId } = render(
+        <EngineProvider initialState={state}>
+          <EndScreen />
+        </EngineProvider>
+      );
+
+      const playerOutcomes = getAllByTestId("text-player-outcome");
+      expect(playerOutcomes).toHaveLength(3);
+    });
+
+    test("lists the players in order", () => {
+      const { getAllByTestId } = render(
+        <EngineProvider initialState={state}>
+          <EndScreen />
+        </EngineProvider>
+      );
+
+      const playerOutcomes = getAllByTestId("text-player-outcome");
+      expect(playerOutcomes[0]).toHaveTextContent(`Player 3`);
+      expect(playerOutcomes[1]).toHaveTextContent(`Player 1`);
+      expect(playerOutcomes[2]).toHaveTextContent(`Player 2`);
     });
   });
 });
--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -16,15 +16,24 @@ export const EndScreen: React.FC = () => {
     );
   }
 
-  const winner = players
-    .filter((p) => p.completedPhase === 10)
-    .sort((a, b) => a.lastCommittedScore - b.lastCommittedScore)[0];
+  const sortedPlayers = players.sort((a, b) => {
+    if (a.completedPhase > b.completedPhase) return 1;
+    if (b.completedPhase > a.completedPhase) return -1;
+    return a.lastCommittedScore - b.lastCommittedScore;
+  });
+
+  const winner = sortedPlayers[0];
 
   return (
     <div>
-      <h1>Congratulations {winner.name || `Player ${winner.playerId}`}!</h1>
-      {players.map((p) => (
-        <div>
+      <h1 data-testid="text-congrats">
+        Congratulations {winner.name || `Player ${winner.playerId}`}!
+      </h1>
+      {sortedPlayers.map((p) => (
+        <div
+          key={`player-outcome-${p.playerId}`}
+          data-testid="text-player-outcome"
+        >
           {p.name || `Player ${p.playerId}`} completed Phase {p.completedPhase}{" "}
           with {p.lastCommittedScore} points.
         </div>
